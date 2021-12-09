6e77f73ff8c031961a57a793493295cc5b6520a7
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-09 18:18:49 +0100

    🔨  Import component in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Navbar from "./Components/Navbar/Navbar";
+import Sidebar from "./Components/Sidebar/Sidebar";
+
 function App() {
-  return (
-    <div className="App">
-   
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navbar />
+			<Sidebar />
+		</div>
+	);
 }
 
 export default App;
