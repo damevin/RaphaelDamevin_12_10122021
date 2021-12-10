39c548262151550e7a4c65ed582d20f55d01d85e
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-10 13:08:49 +0100

    🚧 WIP: Home container

---
+++ src/Containers/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Home.scss";
+import Header from "../../Components/Header/Header";
+
+export default function Home() {
+	return (
+		<main className="home">
+			<Header  />
+		</main>
+	);
+}
