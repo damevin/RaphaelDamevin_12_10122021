edc8ad0af8b5cee80df06153ab83a6f8c4836686
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-10 13:09:51 +0100

    🐛  Fix header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,7 @@ import "./Header.scss";
 export default function Header() {
 	return (
 		<header className="header">
-			<h1 className="header__title">Bonjour user/h1>
+			<h1 className="header__title">Bonjour user</h1>
 			<p className="header__subtitle">Félicitations ! Vous avez explosé vos objectifs hier 👏</p>
 		</header>
 	);
