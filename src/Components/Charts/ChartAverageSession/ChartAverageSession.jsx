a4c0d659b0ab01145296886960482a3a7864f334
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-10 16:27:51 +0100

    🚧  WIP: New chart component

---
+++ src/Components/Charts/ChartAverageSession/ChartAverageSession.jsx
@@ -0,0 +1,35 @@
+import "./ChartAverageSession.scss";
+import { LineChart, CartesianGrid, XAxis, YAxis, Line, Legend, Tooltip } from "recharts";
+import { useApiGet } from "../../../Hooks/useApi";
+import React from "react";
+
+export default function ChartAverageSession({ userId }) {
+	const { data, loading } = useApiGet("GET_AVERAGE", userId);
+
+	if (data && !loading) {
+  console.log("====")
+  console.log(data)
+  console.log("====")
+		return (
+			<div>
+				<h2>Durée moyenne des sessions</h2>
+				<LineChart
+					width={730}
+					height={250}
+					data={data.data.sessions}
+					margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+				>
+					<CartesianGrid strokeDasharray="3 3" />
+					<XAxis dataKey="day" />
+					<YAxis dataKey="sessionLength" />
+					<Tooltip />
+					<Legend />
+					<Line type="monotone" dataKey="sessionLength"  stroke="#8884d8" />
+				</LineChart>
+			</div>
+		);
+	} else {
+  /* TO DO : Composant qui affiche le chargement */
+  return <span>Loading...</span>
+ }
+}
