1e2608f7ed71ecb077c14a44f7b3b1cc3b21c29b
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-10 16:28:29 +0100

    ✨ Create component for displaying error

---
+++ src/Containers/Error/Error.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Error.scss'
+
+export default function Error() {
+	return (
+		<div className="error">
+			<h1 className="error__title">Utilisateur introuvable 😭</h1>
+		</div>
+	);
+}
