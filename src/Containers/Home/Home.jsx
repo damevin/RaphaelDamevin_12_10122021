67be2c419d5e967a7a2299d1fb9124999c66ba39
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-10 14:26:09 +0100

    🚧 WIP: Components, props, data fetching properly

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,11 +1,21 @@
-import React from "react";
 import "./Home.scss";
+import { useApiGet } from "../../Hooks/useApi";
+import { useParams } from "react-router-dom";
 import Header from "../../Components/Header/Header";
+import React from "react";
 
 export default function Home() {
-	return (
-		<main className="home">
-			<Header  />
-		</main>
-	);
+	const { id } = useParams();
+	const { data, error, loading } = useApiGet("GET_INFORMATIONS", id);
+
+	if (!loading && data) {
+		return (
+			<main className="home">
+				<span>{data.data.userInfos.firstName}</span>
+				<span>{data.data.userInfos.lastName}</span>
+			</main>
+		);
+	} else {
+		return <></>;
+	}
 }
