78721c8f6bc9f2bd1a4831250fe75fdf67d66a2b
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-10 12:35:05 +0100

    ⚗️  Try to get data correctly depends on useApiGet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import Navbar from "./Components/Navbar/Navbar";
 import Sidebar from "./Components/Sidebar/Sidebar";
+import { useApiGet } from "./Hooks/useApi";
 
 function App() {
+	const callApi = useApiGet("http://localhost:3001", "GET_PERFORMANCE", 18);
+	if (!callApi.loading) console.log(callApi.data);
+
 	return (
 		<div className="App">
 			<Navbar />
