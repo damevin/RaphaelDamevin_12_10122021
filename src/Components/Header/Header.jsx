760ceb73f461216ac4aae4f6ea82344f55276e34
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-10 13:09:35 +0100

    🚧 WIP: Header component

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.scss";
+
+export default function Header() {
+	return (
+		<header className="header">
+			<h1 className="header__title">Bonjour user/h1>
+			<p className="header__subtitle">Félicitations ! Vous avez explosé vos objectifs hier 👏</p>
+		</header>
+	);
+}
