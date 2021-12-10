0f7b20ec7d46a69a0b9378d8f7d3fde65a04ae07
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-10 13:08:16 +0100

    🎨  Import router on file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import Navbar from "./Components/Navbar/Navbar";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import { useApiGet } from "./Hooks/useApi";
+import Router from "./router";
 
 function App() {
-	const callApi = useApiGet("http://localhost:3001", "GET_PERFORMANCE", 18);
+	const callApi = useApiGet("http://localhost:3001", "GET_INFORMATIONS", 18);
 	if (!callApi.loading) console.log(callApi.data);
 
 	return (
 		<div className="App">
 			<Navbar />
+			<Router></Router>
 			<Sidebar />
 		</div>
 	);
