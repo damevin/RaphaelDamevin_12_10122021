35d0b0d35471a6372fd00da11fbdab93e0961cca
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-10 16:28:12 +0100

    ⚡️  Improve header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Header.scss";
 
-export default function Header({user}) {
+export default function Header({ userFirstName, userLastName }) {
 	return (
 		<header className="header">
-			<h1 className="header__title">Bonjour user </h1>
+			<h1 className="header__title">Bonjour <span className="header__title__child">{userFirstName}</span></h1>
 			<p className="header__subtitle">Félicitations ! Vous avez explosé vos objectifs hier 👏</p>
 		</header>
 	);
