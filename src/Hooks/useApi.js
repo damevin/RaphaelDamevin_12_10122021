ae6445f703634b76100e513bcbdddbef32dea176
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-10 14:25:16 +0100

    🐛  Fix useEffect misusage

--- src/Hooks/useApi.js
+++ src/Hooks/useApi.js
@@ -1,54 +1,60 @@
 import { useState, useEffect } from "react";
 
+const API = "http://localhost:3001";
+
 /**
-	* Util function for get correct query
-	* @param {String} url | URL where we fetch data
-	* @param {String} query | Query name 
-	* @param {Number} userId | User id
-	* @returns {String}
-	*/
-const getQuery = (url, query, userId) => {
-	switch (query) {
-		case "GET_INFORMATIONS":
-			return `${url}/user/${userId}`;
+ * Factory for retrive valid query depends on his service
+ * @param {*} service
+ * @param {*} userId
+ * @returns
+ */
+function getQueryByServices(service, userId) {
+	switch (service) {
 		case "GET_ACTIVITY":
-			return `${url}/user/${userId}/activity`;
+			return `user/${userId}/activity`;
 		case "GET_PERFORMANCE":
-			return `${url}/user/${userId}/performance`;
+			return `user/${userId}/performance`;
+		case "GET_INFORMATIONS":
+			return `user/${userId}`;
 		default:
-			return "/";
+			return null;
 	}
-};
+}
 
 /**
  * Custom hook for get data from an API
  * @param {String} url | URL where we fetch data
  * @returns {Object}
  */
-export const useApiGet = (url, query, userId) => {
+export const useApiGet = (service, userId) => {
 	const [status, setStatus] = useState(0);
 	const [statusText, setStatusText] = useState("");
 	const [data, setData] = useState();
 	const [error, setError] = useState();
 	const [loading, setLoading] = useState();
 
-	const getApiData = async () => {
-		setLoading(true);
-		try {
-			const apiResponse = await fetch(getQuery(url, query, userId));
-			const json = await apiResponse.json();
-			setStatus(apiResponse.status);
-			setStatusText(apiResponse.statusText);
-			setData(json);
-		} catch (error) {
-			setError(error);
-		}
-		setLoading(false);
-	};
+	const endpoint = getQueryByServices(service, userId);
 
 	useEffect(() => {
+		if (!endpoint) return;
+
+		setLoading(true);
+		const getApiData = async () => {
+			setLoading(true);
+			try {
+				const url = `${API}/${endpoint}`;
+				const apiResponse = await fetch(url);
+				const json = await apiResponse.json();
+				setStatus(apiResponse.status);
+				setStatusText(apiResponse.statusText);
+				setData(json);
+			} catch (error) {
+				setError(error);
+			}
+			setLoading(false);
+		};
 		getApiData();
-	}, []);
+	}, [endpoint, service, userId]);
 
 	return { status, statusText, data, error, loading };
 };
