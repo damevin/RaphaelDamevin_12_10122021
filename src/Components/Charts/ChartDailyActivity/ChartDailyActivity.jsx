e9381aaa8da4ffcbc64d82427b4fea3cca41192b
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-17 12:31:43 +0100

    💄  Fix component layout placement

--- src/Components/Charts/ChartDailyActivity/ChartDailyActivity.jsx
+++ src/Components/Charts/ChartDailyActivity/ChartDailyActivity.jsx
@@ -1,4 +1,5 @@
 import "../../../style/variables.scss";
+import './ChartDailyActivity.scss'
 import { Bar, BarChart, CartesianGrid, Tooltip, XAxis, YAxis } from "recharts";
 import { ReactComponent as Dot } from "./dot.svg";
 import { useApiGet } from "../../../Hooks/useApi";
@@ -11,21 +12,20 @@ export default function ChartDailyActivity({ userId }) {
 	if (data && !loading) {
 		const sessions = data.data.sessions
 		return (
-			<>
+			<div className="chart__dailyActivity">
 				Activité quotidienne
 				<Dot fill=""></Dot>
 				Poids (kg)
 				<Dot fill={"red"}></Dot>
 				Calories brûlées (kCal)
-				<BarChart width={730} height={250} data={sessions}>
+				<BarChart width={730} height={180} data={sessions}>
 					<CartesianGrid strokeDasharray="3 3" />
 					<XAxis dataKey="day" />
-					<YAxis />
 					<Tooltip />
 					<Bar dataKey="kilogram" fill="$primary" />
 					<Bar dataKey="calories" fill="$secondary" />
 				</BarChart>
-			</>
+			</div>
 		);
 	} else {
 		return <Error />;
