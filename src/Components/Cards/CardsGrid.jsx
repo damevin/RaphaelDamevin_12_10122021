dcf7f9fd0847ebeda43ee1a7d6cbed5528148db9
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-17 18:05:14 +0100

    ✨  Create component for cards keys grid

---
+++ src/Components/Cards/CardsGrid.jsx
@@ -0,0 +1,22 @@
+import "./CardsGrid.scss";
+import { useApiGet } from "../../Hooks/useApi";
+import CardKeyInfos from "../CardKeyInfos/CardKeyInfos";
+import React from "react";
+
+export default function Cards({ userId }) {
+	console.log("userId", userId);
+	const { data } = useApiGet("GET_KEYS", userId);
+
+	if (data) {
+		return (
+			<section className="cardGrid">
+				<CardKeyInfos type="Calories" value={data.data.keyData.calorieCount} />
+				<CardKeyInfos type="Protéines" value={data.data.keyData.proteinCount} />
+				<CardKeyInfos type="Glucides" value={data.data.keyData.carbohydrateCount} />
+				<CardKeyInfos type="Lipides" value={data.data.keyData.lipidCount} />
+			</section>
+		);
+	} else {
+		return null;
+	}
+}
