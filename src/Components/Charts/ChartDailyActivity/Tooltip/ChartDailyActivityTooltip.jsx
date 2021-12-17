2c3698060f338651c91207a96a27a02bfb674701
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-17 16:12:43 +0100

    ♻️  Refactoring tooltip

--- src/Components/Charts/ChartDailyActivity/Tooltip/ChartDailyActivityTooltip.jsx
+++ src/Components/Charts/ChartDailyActivity/Tooltip/ChartDailyActivityTooltip.jsx
@@ -1,13 +1,13 @@
-import "./CustomTooltip.scss";
+import "./ChartDailyActivityTooltip.scoped.scss";
 
 import React from "react";
 
 export default function CustomTooltip({ payload, active, label }) {
 	if (active && payload && payload.length) {
 		return (
-			<div className="tooltip">
-				<p className="tooltip__value">{`${payload[0].value}`}kg</p>
-				<p className="tooltip__value">{`${payload[1].value}`}Kcal</p>
+			<div className="tooltip__dailyActivity">
+				<p>{`${payload[0].value}`}kg</p>
+				<p>{`${payload[1].value}`}Kcal</p>
 			</div>
 		);
 	} else {
