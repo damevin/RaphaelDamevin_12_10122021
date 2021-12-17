e70aa9b5de6e205309db777338fbeba224067fe7
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-17 18:03:41 +0100

    ✨  Create component for user keys informations

--- src/Components/CardKeyInfos/CardKeyInfos.jsx
+++ src/Components/CardKeyInfos/CardKeyInfos.jsx
@@ -1,15 +1,32 @@
 import React from "react";
-import { useApiGet } from "../../Hooks/useApi";
+import "./CardKeyInfos.scss";
+import iconCalories from "./icons/iconCalories.svg";
+import iconCarbs from "./icons/iconCarbs.svg";
+import iconFat from "./icons/iconFat.svg";
+import iconProteins from "./icons/iconProteins.svg";
 
-export default function CardKeyInfos({ userId }) {
-	const { data, loading } = useApiGet("GET_KEYS", userId);
-	return <div></div>;
-	/* if (data && !loading) {
-		const keyNumbers = data.data.keyData;
-  console.log(keyNumbers);
-		const keyNumbersArr = Object.values(keyNumbers);
-		const keyNumbersEntries = ["", "", "", ""];
-		console.log(keyNumbersArr);
-		
-	} */
+const iconsTypes = {
+	Calories: iconCalories,
+	Glucides: iconCarbs,
+	Protéines: iconProteins,
+	Lipides: iconFat,
+};
+
+const unitTypes = {
+	Calories: "Kcal",
+	Glucides: "g",
+	Protéines: "g",
+	Lipides: "g",
+};
+
+export default function CardKeyInfos({ type, value }) {
+	return (
+		<article className="cardKeys">
+			<img className="cardKeys__logo" src={iconsTypes[type]} alt="" />
+			<div className="cardKeys__section">
+				<span className="cardKeys__value">{`${value}${unitTypes[type]}`}</span>
+				<span className="cardKeys__type">{type}</span>
+			</div>
+		</article>
+	);
 }
