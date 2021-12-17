c569a5ff858b180ea61609ef8ba4dec0a68aa6d3
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-17 12:32:09 +0100

    🚧  Formated code, component in progress...

--- src/Components/Charts/ChartAverageSession/ChartAverageSession.jsx
+++ src/Components/Charts/ChartAverageSession/ChartAverageSession.jsx
@@ -7,27 +7,22 @@ export default function ChartAverageSession({ userId }) {
 	const { data, loading } = useApiGet("GET_AVERAGE", userId);
 
 	if (data && !loading) {
-  const sessions = data.data.sessions
+		const sessions = data.data.sessions;
 		return (
-			<div>
+			<div className="chart__averageSession">
 				<h2>Durée moyenne des sessions</h2>
-				<LineChart
-					width={730}
-					height={250}
-					data={sessions}
-					margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-				>
+				<LineChart width={250} height={180} data={sessions}>
 					<CartesianGrid strokeDasharray="3 3" />
 					<XAxis dataKey="day" />
 					<YAxis dataKey="sessionLength" />
 					<Tooltip />
 					<Legend />
-					<Line type="monotone" dataKey="sessionLength"  stroke="#8884d8" />
+					<Line type="monotone" dataKey="sessionLength" stroke="#8884d8" />
 				</LineChart>
 			</div>
 		);
 	} else {
-  /* TO DO : Composant qui affiche le chargement */
-  return <span>Loading...</span>
- }
+		/* TO DO : Composant qui affiche le chargement */
+		return <span>Loading...</span>;
+	}
 }
