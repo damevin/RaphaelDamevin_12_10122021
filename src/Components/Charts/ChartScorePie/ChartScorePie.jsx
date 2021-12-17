78c352f845da53840a2c699f8215d6fb627ebf4f
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-17 12:31:26 +0100

    🚧  WIP: Score pie chart

--- src/Components/Charts/ChartScorePie/ChartScorePie.jsx
+++ src/Components/Charts/ChartScorePie/ChartScorePie.jsx
@@ -7,20 +7,40 @@ export default function ChartScorePie({ userId }) {
 	const { data, loading } = useApiGet("GET_INFORMATIONS", userId);
 
 	if (data && !loading) {
-		let dailyScore = data.data.todayScore;
-
+		let dailyScore = data.data.score;
 		const dailyData = [
-			{ name: "finished", value: dailyScore, fillColor: "red" },
-			{ name: "inProgress", value: 1 - dailyScore, fillColor: "blue" },
+			{ name: "finished", value: dailyScore, fillColor: "#ff0101" },
+			{ name: "inProgress", value: 1 - dailyScore, fillColor: "transparent" },
 		];
 		return (
-			<PieChart width={730} height={250}>
-				<Pie data={dailyData} dataKey="value" cx="50%" cy="50%"  fill="#8884d8">
-					{dailyData.map((entry, index) => (
-						<Cell key={`${index}`} fill={entry.fillColor}  />
-					))}
-				</Pie>
-			</PieChart>
+			<div className="chart__score">
+				<h3 className="chart__score__title">Score</h3>
+				<main className="chart__score__main">
+					<PieChart width={150} height={150}>
+						<Pie
+							data={dailyData}
+							dataKey="value"
+							innerRadius={70}
+							outerRadius={80}
+							startAngle={90}
+							endAngle={460}
+							paddingAngle={5}
+							cx={"50%"}
+							cy={"50%"}
+						>
+							{dailyData.map((entry, index) => (
+								<Cell key={`${index}`} fill={entry.fillColor} cornerRadius="50%" className="chart__pie" />
+							))}
+						</Pie>
+					</PieChart>
+					<section className="chart__score__legend">
+						<p className="chart__score__legend__details">
+							<span>{dailyScore * 100}%</span>
+							<br /> de votre objectif
+						</p>
+					</section>
+				</main>
+			</div>
 		);
 	} else {
 		return <p>Loading..</p>;
