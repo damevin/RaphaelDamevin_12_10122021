ad4654f69476ca243d7e99c4724d29075563f4ac
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-20 16:39:02 +0100

    💄  Update layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Navbar from "./Components/Navbar/Navbar";
 import Router from "./router";
 import Sidebar from "./Components/Sidebar/Sidebar";
+import "../src/style/layout.scss";
 
 function App() {
 	return (
-		<div className="App">
+		<div className="layout">
 			<Navbar />
-			<Router></Router>
+			<Router />
 			<Sidebar />
 		</div>
 	);
