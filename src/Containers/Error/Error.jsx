2dfc75d7dfc9aec2a36febc66e234c73b219885d
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-12-20 16:42:10 +0100

    💄  Improve error page

--- src/Containers/Error/Error.jsx
+++ src/Containers/Error/Error.jsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function Error({ details }) {
 	return (
 		<div className="error">
-			<h1 className="error__title">Utilisateur introuvable 😭</h1>
+			<h1 className="error__title">Erreur... 😭</h1>
 			<span className="error__description">Error : {details}</span>
 		</div>
 	);
